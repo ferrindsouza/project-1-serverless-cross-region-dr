0ffeff7926f35e1171bede2a5d94e4d03a6fbeab
ferrindsouza <fmc.dsouza@gmail.com>
2025-06-02 20:22:09 +0530

    Removed unused imports and added support for decimal values

--- read_function.py
+++ read_function.py
@@ -3,16 +3,23 @@
 import json
 import boto3
 import os
-from boto3.dynamodb.conditions import Key
+from decimal import Decimal
 
+#  get region and table name from environment variables with fallbacks
+region = os.environ.get('REGION', 'us-east-1')
+table_name = os.environ.get('TABLE_NAME', 'HighAvailabilityTable')
+dynamodb = boto3.resource('dynamodb', region_name=region)
+table = dynamodb.Table(table_name)
+
+def decimal_encoder(obj):
+    if isinstance(obj, Decimal):
+        if obj % 1 == 0:
+            return int(obj)
+        else:
+            return float(obj)
+    raise TypeError(f"Object of type {obj.__class__.__name__} is not JSON serializable")
 
 def lambda_handler(event, context):
-    #  get region and table name from environment variables with fallbacks
-    region = os.environ.get('REGION', 'us-east-1')
-    table_name = os.environ.get('TABLE_NAME', 'HighAvailabilityTable')
-    dynamodb = boto3.resource('dynamodb', region_name=region)
-    table = dynamodb.Table(table_name)
-
     try:
         items = []
         response = table.scan()
@@ -28,7 +35,7 @@ def lambda_handler(event, context):
                 'Content-Type': 'application/json',
                 'Access-Control-Allow-Origin': '*',  # Enable CORS
             },
-            'body': json.dumps(items)  # Return the items array
+            'body': json.dumps(items, default=decimal_encoder)  # Return the items array
         }
     except Exception as e:
         print(f"Error: {str(e)}")
